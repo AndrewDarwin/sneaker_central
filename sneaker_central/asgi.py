2f2d045e91cd3b4d5c98744f3af4660de64ada92
AndrewDarwin <djdarz@gmail.com>
2022-01-03 14:32:36 +0000

    initial commit

---
+++ sneaker_central/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sneaker_central project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sneaker_central.settings')
+
+application = get_asgi_application()
