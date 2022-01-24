77af2272fe8a081cd8c017e738503cb56403ca0a
AndrewDarwin <djdarz@gmail.com>
2022-01-24 14:27:01 +0000

    removed secret key and set debug

--- sneaker_central/settings.py
+++ sneaker_central/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-htm9l-acgb_)d09*xv4zg_us9+%0baf54taw6++zdetz_)a((6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sneakercentral.herokuapp.com', 'localhost']
 
